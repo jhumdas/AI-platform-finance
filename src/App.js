814b6eabda7811e2622cc4e7bffe6b389021b2ae
jhumdas <dasjhuma0099@gmail.com>
2025-10-26 14:31:22 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-
-
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/dashboard";
-import ChatWidget from "./components/ChatWidget";
 
 export default function App() {
   return (
-    <Router>
+    <>
       <header className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-          <h1 className="text-2xl font-semibold md:text-xl">Financial Dashboard</h1>
+          <h1 className="text-2xl font-semibold md:text-xl">
+            Financial Dashboard
+          </h1>
           <button className="text-white text-lg rounded py-2 px-8 font-semibold bg-red-500">
             Logout
           </button>
@@ -20,11 +18,8 @@ export default function App() {
       </header>
 
       <main className="min-h-screen max-w-7xl mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/chat" element={<ChatWidget />} />
-        </Routes>
+        <Dashboard />
       </main>
-    </Router>
+    </>
   );
 }
